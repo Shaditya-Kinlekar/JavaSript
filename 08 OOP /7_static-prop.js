6953f1286cb7bdf7e0eb3d4070efece64695c543
Shaditya Kinlekar <shadityakinlekar20@gmail.com>
2024-02-01 16:35:00 +0530

    updated static-prop.js

--- 08 OOP /7_static-prop.js
+++ 08 OOP /7_static-prop.js
@@ -1,6 +1,7 @@
 class User {
-  constructor(username) {
+  constructor(username, age) {
     this.username = username;
+    this.age = age;
   }
 
   logMe() {
@@ -15,17 +16,21 @@ class User {
   }
 }
 
-const u1 = new User('John');
+const u1 = new User('John', 23);
 u1.logMe();
+console.log(u1);
 
+// The "super" keyword in the constructor method is used to call the constructor of the parent class (in this case, the User class). It is used to pass the (username, age) parameter to the constructor of the User class, allowing the User class to initialize its own properties using the passed (username, age). This is a common practice when creating subclasses that extend from a parent class.
 class Teacher extends User {
-  constructor(username, email) {
-    super(username);
+  constructor(username, age, email) {
+    super(username, age);
     this.email = email;
   }
 }
 
-const u2 = new Teacher('Kate', 'kate@email');
+const u2 = new Teacher('Kate', 40, 'kate@email');
 // console.log(u2.createID());  // error because createID is not available on instanceof a class
 
 console.log(Teacher.createID()); // This will work because static methods are inherited in subclasses, so Teacher.createID() is the same as User.createID().
+
+console.log(u2);
