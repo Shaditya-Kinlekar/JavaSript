d4b1d9b2b0b4ed58ece26e86292cf503b5886807
Shaditya Kinlekar <shadityakinlekar20@gmail.com>
2024-02-24 21:21:07 +0530

    updated Project 8

--- 05 Data Structures/3 Spread operator/script.js
+++ 05 Data Structures/3 Spread operator/script.js
@@ -34,9 +34,7 @@ const restaurant = {
   },
   orderPasta: function (ing1, ing2, ing3) {
     // function which accepts multiple arguments
-    console.log(
-      `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
-    );
+    console.log(`Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`);
   },
 };
 
@@ -93,5 +91,6 @@ console.log(newRestaurant);
 // shallow copy
 const restaurantCopy = { ...restaurant };
 restaurantCopy.name = 'Ristornate Roma';
+
 console.log(restaurantCopy.name);
 console.log(restaurant.name);
